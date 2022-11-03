0b5d2e4b2db166d5262e0816b593d0cf94ac629a
minju <bbak0402dd@gmail.com>
2022-11-03 19:19:36 +0900

    [#1] h2 데이터베이스 설정

---
+++ src/main/resources/data-h2.sql
@@ -0,0 +1,6 @@
+INSERT INTO users(seq, email, passwd)
+VALUES (null, 'test00@gmail.com', 'abc123!@#');
+INSERT INTO users(seq, email, passwd)
+VALUES (null, 'test01@gmail.com', 'bcd234@#$');
+INSERT INTO users(seq, email, passwd)
+VALUES (null, 'test02@gmail.com', 'cde345#$%');
